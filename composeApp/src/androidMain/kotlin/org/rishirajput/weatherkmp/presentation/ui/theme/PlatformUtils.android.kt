696bfa8f05c71e88a0dd51cb5d74ac525f513065
rishirajput <rishi_raj@live.com>
2025-01-03 00:10:35 +0530

    create actual functions for platform themes

---
+++ composeApp/src/androidMain/kotlin/org/rishirajput/weatherkmp/presentation/ui/theme/PlatformUtils.android.kt
@@ -0,0 +1,13 @@
+package org.rishirajput.weatherkmp.presentation.ui.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun isSystemInDarkTheme(): Boolean {
+    TODO("Not yet implemented")
+}
+
+@Composable
+actual fun isDynamicColorSupported(): Boolean {
+    TODO("Not yet implemented")
+}
---
+++ composeApp/src/commonMain/kotlin/org/rishirajput/weatherkmp/presentation/ui/theme/PlatformUtils.kt
@@ -0,0 +1,10 @@
+
+package org.rishirajput.weatherkmp.presentation.ui.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+expect fun isSystemInDarkTheme(): Boolean
+
+@Composable
+expect fun isDynamicColorSupported(): Boolean
