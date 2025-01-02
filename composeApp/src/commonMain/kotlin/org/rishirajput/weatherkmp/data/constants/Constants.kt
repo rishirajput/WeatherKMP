e1fa1f88b23a7c1ef4f6b9a530ff4f307aa7c681
rishirajput <rishi_raj@live.com>
2025-01-02 19:12:22 +0530

    create Constants.kt

---
+++ composeApp/src/commonMain/kotlin/org/rishirajput/weatherkmp/data/constants/Constants.kt
@@ -0,0 +1,8 @@
+package org.rishirajput.weatherkmp.data.constants
+
+/**
+ * Object to hold constant values used throughout the application.
+ */
+object Constants {
+    const val DEBOUNCE_DELAY = 400L
+}
