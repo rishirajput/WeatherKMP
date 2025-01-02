23ba6fd74f42c84889441dda9ab51165fbc1fbbe
rishirajput <rishi_raj@live.com>
2025-01-02 22:02:01 +0530

    update dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -90,6 +90,7 @@ kotlin {
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(libs.ktor.client.cio)
 
             implementation(libs.coil.compose)
             implementation(libs.coil.network.ktor)
