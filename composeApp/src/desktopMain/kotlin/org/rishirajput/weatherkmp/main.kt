d95a5efa72ffb39816523925f4ca5e4c9f448f47
rishirajput <rishi_raj@live.com>
2025-01-03 15:08:49 +0530

    setup koin for desktop application

--- composeApp/src/desktopMain/kotlin/org/rishirajput/weatherkmp/main.kt
+++ composeApp/src/desktopMain/kotlin/org/rishirajput/weatherkmp/main.kt
@@ -2,8 +2,14 @@ package org.rishirajput.weatherkmp
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.koin.core.context.startKoin
+import org.rishirajput.weatherkmp.di.dataModule
+import org.rishirajput.weatherkmp.di.viewModelModule
 
 fun main() = application {
+    startKoin {
+        modules(dataModule, viewModelModule)
+    }
     Window(
         onCloseRequest = ::exitApplication,
         title = "Weather",
