d14ed028cdce57a7c5c0fd8804edcbc22b4e89f6
rishirajput <rishi_raj@live.com>
2025-01-03 16:54:23 +0530

    initialize koin for wasmJsMain

--- composeApp/src/wasmJsMain/kotlin/org/rishirajput/weatherkmp/main.kt
+++ composeApp/src/wasmJsMain/kotlin/org/rishirajput/weatherkmp/main.kt
@@ -3,9 +3,15 @@ package org.rishirajput.weatherkmp
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import org.koin.core.context.GlobalContext.startKoin
+import org.rishirajput.weatherkmp.di.dataModule
+import org.rishirajput.weatherkmp.di.viewModelModule
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    startKoin {
+        modules(dataModule, viewModelModule)
+    }
     ComposeViewport(document.body!!) {
         App()
     }
